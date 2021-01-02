8f6ea0319e9b769e973d7f02cb4456f8ed807da2
Oliver Vogel <oliver@olivervogel.com>
2021-01-02 01:20:25 +0100

    ImageData fix

--- tests/ImageDataTest.php
+++ tests/ImageDataTest.php
@@ -6,29 +6,36 @@ use Intervention\Gif\ImageData;
 
 class ImageDataTest extends BaseTestCase
 {
-    public function testSetGetData()
+    public function testSetGetBlocks()
     {
         $data = new ImageData();
-        $data->setData('foo');
-        $this->assertEquals('foo', $data->getData());
+        $this->assertFalse($data->hasBlocks());
+        $data->addBlock('foo');
+        $this->assertCount(1, $data->getBlocks());
+        $this->assertTrue($data->hasBlocks());
+        $data->addBlock('bar');
+        $this->assertCount(2, $data->getBlocks());
+        $this->assertTrue($data->hasBlocks());
     }
 
     public function testEncoder()
     {
         $data = new ImageData();
-        $data->setLzwMinCodeSize(2);
-        $data->setData("\x8C\x2D\x99\x87\x2A\x1C\xDC\x33\xA0\x02\x75\xEC\x95\xFA\xA8\xDE\x60\x8C\x04\x91\x4C\x01");
+        $data->setLzwMinCodeSize(5);
+        $data->addBlock("\x20\x20\x8E\x64\x69\x9E\x51\xA0\x46\x67\xEB\xBE\x70\x2C\x97\xE9\x3A\xDF\x78\xAE\xDF\x4F\xD4\x40\x8F\x9B\x43\x15\x70\xF0\x7C\xC0\x9D\xB2\x15\x02");
+        $data->addBlock("\x01\x01\x01\x01");
+        $data->addBlock("\x01\x01\x01");
 
-        $result = "\x02\x16\x8C\x2D\x99\x87\x2A\x1C\xDC\x33\xA0\x02\x75\xEC\x95\xFA\xA8\xDE\x60\x8C\x04\x91\x4C\x01\x00";
+        $result = "\x05\x24\x20\x20\x8E\x64\x69\x9E\x51\xA0\x46\x67\xEB\xBE\x70\x2C\x97\xE9\x3A\xDF\x78\xAE\xDF\x4F\xD4\x40\x8F\x9B\x43\x15\x70\xF0\x7C\xC0\x9D\xB2\x15\x02\x04\x01\x01\x01\x01\x03\x01\x01\x01\x00";
         $this->assertEquals($result, $data->encode());
     }
 
     public function testDecode()
     {
-        $source = "\x02\x16\x8C\x2D\x99\x87\x2A\x1C\xDC\x33\xA0\x02\x75\xEC\x95\xFA\xA8\xDE\x60\x8C\x04\x91\x4C\x01\x00";
+        $source = "\x05\x24\x20\x20\x8E\x64\x69\x9E\x51\xA0\x46\x67\xEB\xBE\x70\x2C\x97\xE9\x3A\xDF\x78\xAE\xDF\x4F\xD4\x40\x8F\x9B\x43\x15\x70\xF0\x7C\xC0\x9D\xB2\x15\x02\x04\x01\x01\x01\x01\x03\x01\x01\x01\x00";
         $data = ImageData::decode($this->getTestHandle($source));
         $this->assertInstanceOf(ImageData::class, $data);
-        $this->assertEquals(2, $data->getLzwMinCodeSize());
-        $this->assertEquals("\x8C\x2D\x99\x87\x2A\x1C\xDC\x33\xA0\x02\x75\xEC\x95\xFA\xA8\xDE\x60\x8C\x04\x91\x4C\x01", $data->getData());
+        $this->assertEquals(5, $data->getLzwMinCodeSize());
+        $this->assertCount(3, $data->getBlocks());
     }
 }
