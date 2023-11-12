3327c9d159203833c003bf51c799089fea6f96f2
Oliver Vogel <oliver@olivervogel.com>
2023-11-12 10:02:01 +0100

    Add DisposalMethod descriptions

--- src/DisposalMethod.php
+++ src/DisposalMethod.php
@@ -5,7 +5,7 @@ namespace Intervention\Gif;
 class DisposalMethod
 {
     public const UNDEFINED = 0;
-    public const NONE = 1;
-    public const BACKGROUND = 2;
-    public const PREVIOUS = 3;
+    public const NONE = 1; // overlay each frame in sequence
+    public const BACKGROUND = 2; // clear to background (as indicated by the logical screen descriptor)
+    public const PREVIOUS = 3; // restore the canvas to its previous state
 }
